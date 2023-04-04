d7ff2d8d51ccf3f2a6a215b572215bbb91380dcf
jjlin03 <jayyxlin@gmail.com>
2023-04-04 04:30:10 -0400

    no idea

--- competition_runner.py
+++ competition_runner.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 if __name__ == "__main__":
-    agents = [LemonadeAgent(), LemonadeAgent(), StickAgent(2)]
+    agents = [LemonadeAgent(), LemonadeAgent(False), StickAgent(2)]
     total_u = np.zeros(3)
     for _ in range(10):
         actions = [agent.play_action() for agent in agents]
@@ -15,7 +15,10 @@ if __name__ == "__main__":
         for i, res in enumerate(results):
             agents[i].update_actions(res)
         total_u += utils
+        # print(agents[2].my_actions[-1])
     s = ''
     for i in range(3):
         s += f'Agent {agents[i].name} got utility {total_u[i]} '
     print(s)
+    print(agents[0].q_table)
+    print(np.count_nonzero(agents[0].q_table - 12)) # agent is not learning
--- my_rl_lemonade_agent.py
+++ my_rl_lemonade_agent.py
@@ -3,6 +3,7 @@ from lemonade_result import LemonadeResult
 from lemonade_helpers import get_utility
 import random
 import numpy as np
+from sample_agent import StickAgent
 
 def getmindist(x, y):
     return min((x-y) % 12, (y-x) % 12)
@@ -13,8 +14,8 @@ class LemonadeAgent(Agent):
         super().__init__()
         self.name = 'UConnDub'
         self.num_states = 1296
-        self.q_table = np.array([[12 for _ in range(12)] for _ in range(self.num_states)])
-        self.discount_rate = 0.9
+        self.q_table = np.array([[12 for _ in range(12)] for _ in range(self.num_states)], dtype=float)
+        self.discount_rate = 0.95
         self.dev_tolerance = 2
         self.state = 0
         self.threshold = 0
@@ -30,13 +31,14 @@ class LemonadeAgent(Agent):
         state = self.state # changed
         action = np.argmax(self.q_table[state])
         self.counter += 1
+        # print(f"action counter: {self.counter}")
         return action # returns an int 0-11
 
     def update(self, result: LemonadeResult):
         # TODO: Enter any logic you'd like to include in the update. Please check the base class update_actions before
         # part (a)
         # calculate f_i0, f_ij, l_i for all players and threshold B
-        if(self.counter > 102):
+        if self.counter > 102:
             del self.my_actions[0]
             del self.my_rewards[0]
             del self.opponent_1_actions[0]
@@ -51,17 +53,18 @@ class LemonadeAgent(Agent):
         if self.counter >= 3:
             self.threshold = 0
             Gamma = 0
-            for i in range(2, self.counter):
-                Gamma += self.discount_rate**(self.counter - 1 - i)
+            for k in range(2, self.counter):
+                Gamma += self.discount_rate**(self.counter - 1 - k)
             for k in range(2, self.counter):
                 self.threshold -= (self.discount_rate**(self.counter-1-k)/Gamma)
-                l_1 -= (self.discount_rate**(self.counter-1-k)/Gamma) * getmindist(self.opponent_1_actions[k], self.opponent_1_actions[k-1])**self.dev_tolerance
-                l_2 -= (self.discount_rate**(self.counter-1-k)/Gamma) * getmindist(self.opponent_2_actions[k], self.opponent_2_actions[k-1])**self.dev_tolerance
-                f_10 -= (self.discount_rate**(self.counter-1-k)/Gamma) * getmindist(self.opponent_1_actions[k], ((self.my_actions[k-1] + 6) % 12))**self.dev_tolerance
-                f_12 -= (self.discount_rate**(self.counter-1-k)/Gamma) * getmindist(self.opponent_1_actions[k], ((self.opponent_2_actions[k-1] + 6) % 12))**self.dev_tolerance
-                f_20 -= (self.discount_rate**(self.counter-1-k)/Gamma) * getmindist(self.opponent_2_actions[k], ((self.my_actions[k-1] + 6) % 12))**self.dev_tolerance
-                f_21 -= (self.discount_rate**(self.counter-1-k)/Gamma) * getmindist(self.opponent_2_actions[k], ((self.opponent_1_actions[k-1] + 6) % 12))**self.dev_tolerance
+                l_1 -= ((self.discount_rate**(self.counter-1-k))/Gamma) * getmindist(self.opponent_1_actions[k], self.opponent_1_actions[k-1])**self.dev_tolerance
+                l_2 -= ((self.discount_rate**(self.counter-1-k))/Gamma) * getmindist(self.opponent_2_actions[k], self.opponent_2_actions[k-1])**self.dev_tolerance
+                f_10 -= ((self.discount_rate**(self.counter-1-k))/Gamma) * getmindist(self.opponent_1_actions[k], ((self.my_actions[k-1] + 6) % 12))**self.dev_tolerance
+                f_12 -= ((self.discount_rate**(self.counter-1-k))/Gamma) * getmindist(self.opponent_1_actions[k], ((self.opponent_2_actions[k-1] + 6) % 12))**self.dev_tolerance
+                f_20 -= ((self.discount_rate**(self.counter-1-k))/Gamma) * getmindist(self.opponent_2_actions[k], ((self.my_actions[k-1] + 6) % 12))**self.dev_tolerance
+                f_21 -= ((self.discount_rate**(self.counter-1-k))/Gamma) * getmindist(self.opponent_2_actions[k], ((self.opponent_1_actions[k-1] + 6) % 12))**self.dev_tolerance
             self.threshold *= 6**(self.dev_tolerance)
+            self.threshold *= 0.4
         else:
             l_1 = -1
             l_2 = -1
@@ -69,7 +72,16 @@ class LemonadeAgent(Agent):
             f_12 = -1
             f_20 = -1
             f_21 = -1
-            
+        
+        # print(f"threshold: {self.threshold}")
+        # print("opponent 1")
+        # print(f"f_10: {f_10}")
+        # print(f"f_12: {f_12}")
+        # print(f"l_1: {l_1}")
+        # print("opponent 2")
+        # print(f"f_20: {f_20}")
+        # print(f"f_21: {f_21}")
+        # print(f"l_2: {l_2}")
         if f_10 >= self.threshold:
             self.opponent_states[0] = 0
         elif f_12 >= self.threshold:
@@ -90,11 +102,9 @@ class LemonadeAgent(Agent):
 
         # update state
         old_state = self.state
-        self.state = 0
         l = len(self.opponent_1_actions)
         temp1 = 0
         for i in range(2):
-            # based on f_ij
             temp1 *= 4
             temp1 += self.opponent_states[i]
         temp2 = 0
@@ -111,48 +121,56 @@ class LemonadeAgent(Agent):
             temp3 += (self.opponent_2_actions[-i] // 4)
         self.state = (temp1*9 + temp2)*9 + temp3
 
-        self.q_table[old_state, self.my_actions[-1]] += self.learning_rate*(self.my_rewards[-1] + self.discount_rate * np.max(self.q_table[self.state])
-                                                                             - self.q_table[old_state, self.my_actions[-1]])
+        # print(f"curr state: {self.state}, old state: {old_state}") # we are going to different states
+        # print(f"old value: {self.q_table[old_state, self.my_actions[-1]]}")
+        # print(f"reward: {self.my_rewards[-1]}")
+        # why is this ballooning?
+        self.q_table[old_state, self.my_actions[-1]] = self.learning_rate*(self.my_rewards[-1] + self.discount_rate*np.max(self.q_table[self.state])) + (1 - self.learning_rate)*self.q_table[old_state, self.my_actions[-1]]
+        # print(f"max value: {np.max(self.q_table[self.state])}")
+        # print(f"new value: {self.q_table[old_state, self.my_actions[-1]]}")
+        
+                                                                             
     def train(self):
         # play against two versions of itself
         # play 100,000 rounds then reset relavent variables
         print("training")
         adversary1 = LemonadeAgent(False)
         print("no train")
-        adversary2 = LemonadeAgent(False)
+        adversary2 = StickAgent(4)
         print("no train")
         self.threshold = 0
         agents = [self, adversary1, adversary2]
 
-        for loop in range(100000):
+        for loop in range(100): # change back
+            print(loop)
             my_action = np.random.choice(range(12))
             self.my_actions.append(my_action)
             actions = [my_action, adversary1.play_action(), adversary2.play_action()]
             utils = get_utility(actions)
             results = [LemonadeResult(actions, utils, i) for i in range(3)]
-            for agent, result in zip(agents, results):
-                agent.update_actions(result)
+            for i, res in enumerate(results):
+                agents[i].update_actions(res)
+        print(np.count_nonzero(self.q_table - 12))
         print("done")
+        print(self.q_table)
         self.cleanup()
     
     def cleanup(self):
-        qtable = self.q_table
+        # qtable = self.q_table
         self.my_actions = []
         self.my_rewards = []
         self.opponent_1_actions = []
         self.opponent_2_actions = []
-        self.name = 'UConnDub'
-        self.num_states = 1296
-        self.discount_rate = 0.9
-        self.dev_tolerance = 2
-        self.state = 0
-        self.threshold = 0
-        self.learning_rate = 0.5
+        # self.name = 'UConnDub'
+        # self.num_states = 1296
+        # self.discount_rate = 0.9
+        # self.dev_tolerance = 2
+        # self.state = 0
+        # self.threshold = 0
+        # self.learning_rate = 0.5
         self.opponent_states = [0, 0]
         self.counter = 0
-
-
-
+        
 
 class RandomAgent(LemonadeAgent):
     def __init__(self):
